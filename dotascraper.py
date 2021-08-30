bcb824230012cdeb4de8fbcf3d8f688ad18cdb54
Jason Ebblewhite <jasonebblewhite@gmail.com>
2021-08-30 18:08:57 +0100

    Data acquisition and output

--- dotascraper.py
+++ dotascraper.py
@@ -11,9 +11,9 @@ class DotaScraper:
         self.outfile = outfile
 
     def get_matches(self, initial=True):
-        self.driver.get(self.BASE_URL)
-        sleep(2)
         if initial:
+            self.driver.get(self.BASE_URL)
+            sleep(2)
             matches_container = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/div/div/div/div/div/table/tbody')
             matches_full = matches_container.find_elements_by_xpath('./tr')
             self.match_ids = [match.text.split('\n')[0] for match in matches_full]
@@ -22,17 +22,61 @@ class DotaScraper:
             except:
                 self.create_match_ids()
 
+        else:
+            self.read_match_ids()
+            for match in self.match_ids:
+                self.get_match(match)
+            try:
+                self.write_json()
+            except:
+                self.create_json()
+
     def get_match(self,match_id):
         self.driver.get(self.BASE_URL+match_id)
-        sleep(2)
+        sleep(4)
+        # find the winner of the match from the header
         header = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/header/div[1]')
         header_list = header.text.split('\n')
-        print(header_list)
+        winner_name = header_list[0].replace(' Victory', '')
+        # only looking at captains mode games
         if header_list[2] == 'CAPTAINS MODE':
-            radiant_pickbans = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/div/div[1]/div[3]/div')
-            dire_pickbans = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/div/div[1]/div[6]/div')
-            print(radiant_pickbans.text)
+            match_dict_item = {}
+            match_dict_item['match_id'] = match_id
+            # find the picks and bans for both teams by finding their elements
+            radiant_pickbans_container = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/div/div[1]/div[3]/div')
+            dire_pickbans_container = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/div/div[1]/div[6]/div')
+            radiant_pickbans = radiant_pickbans_container.find_elements_by_xpath('./section')
+            dire_pickbans = dire_pickbans_container.find_elements_by_xpath('./section')
+            # determine winning side by name featuring both in header and team container
+            if winner_name in self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/div/div[1]/div[1]/span[1]').text:
+                winner = "radiant"
+            else:
+                winner = "dire"
 
+            match_dict_item['winner'] = winner
+
+            radiant_picks = []
+            dire_picks = []
+            bans = []
+
+            for pickbans in radiant_pickbans:
+                pickorban = pickbans.find_element_by_xpath('./img').get_attribute('src').replace('https://steamcdn-a.akamaihd.net/apps/dota2/images/heroes/', '').replace('_sb.png', '')
+                if "BAN" in pickbans.text:
+                    bans.append(pickorban)
+                else:
+                    radiant_picks.append(pickorban)
+
+            for pickbans in dire_pickbans:
+                pickorban = pickbans.find_element_by_xpath('./img').get_attribute('src').replace('https://steamcdn-a.akamaihd.net/apps/dota2/images/heroes/', '').replace('_sb.png', '')
+                if "BAN" in pickbans.text:
+                    bans.append(pickorban)
+                else:
+                    dire_picks.append(pickorban)
+            match_dict_item['radiant_picks'] = radiant_picks
+            match_dict_item['dire_picks'] = dire_picks
+            match_dict_item['bans'] = bans
+            self.matches.append(match_dict_item)
+        
 
     def create_match_ids(self):
         with open('match_ids.json','w+') as file:
@@ -52,7 +96,8 @@ class DotaScraper:
 
     def create_json(self):
         with open(self.outfile,'w+') as file:
-            json.dump(self.matches, file, indent = 4)
+            matchdict = {'matches':self.matches}
+            json.dump(matchdict, file, indent = 4)
 
     def write_json(self):
         with open(self.outfile,'r+') as file:
--- scrapertest.py
+++ scrapertest.py
@@ -3,4 +3,4 @@ import dotascraper
 if __name__ == '__main__':
     d2scraper = dotascraper.DotaScraper()
     d2scraper.read_match_ids()
-    d2scraper.get_match(d2scraper.match_ids[0])
+    d2scraper.get_matches(initial=False)
